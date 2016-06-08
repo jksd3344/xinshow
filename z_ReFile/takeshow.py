80aeefc8e60ac926e1d6b48741f0079865733aa3
jksd3344 <jiayuguang@qq.com>
2016-06-08 17:08:17 +0800

    2016-06-08 am 50:00

--- z_ReFile/takeshow.py
+++ z_ReFile/takeshow.py
@@ -46,7 +46,7 @@ class TakeShow(object):
 			self.sqlwh   = self.sqlwh%(self.uid)
 			self.cmd     = './adhoc_ctr_feeding.o','1',str(self.ucid),str(self.ShowDays),str(self.oid)
 			print ("self.cmd=%s"%self.cmd)
-			self.host1   = {"host_":"192.168.241.50","port_":17717,"username":"root","password":"Jksd3344","cmd":"cd %s;%s"}%(self.bin1,self.cmd)}
+			self.host1   = {"host_":"192.168.241.50","port_":17717,"username":"root","password":"Jksd3344","cmd":"cd %s;%s"%(self.bin1,self.cmd)}
 			self.host2   = {"host_":"123.57.226.182","port_":22,"username":"root","password":"Jksd3344","cmd":"cd /home/itcast/testy;./sleepTest.o"}
 			self.host3   = {"host_":"123.57.226.182","port_":22,"username":"root","password":"Jksd3344","cmd":"cd /home/itcast/testy;./sleepTest.o"}
 
