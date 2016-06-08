e67274143ff3479a8cbfb0505d7b6ab886c56f6b
jksd3344 <jiayuguang@qq.com>
2016-06-08 17:33:22 +0800

    2016-06-08 am 50:00

--- z_ReFile/takeshow.py
+++ z_ReFile/takeshow.py
@@ -44,7 +44,7 @@ class TakeShow(object):
 			self.hostid  = int((self.pat)[6])
 			self.sqlcom  = self.sqlcom%(self.uid)
 			self.sqlwh   = self.sqlwh%(self.uid)	
-			self.host1   = {"host_":"123.57.226.182","port_":22,"username":"root","password":"Jksd3344","cmd":"cd /home/itcast/testy;./sleepTest.o"}
+			self.host1   = {"host_":"192.168.241.50","port_":17717,"username":"zzg","password":"hZ4o7ZpG888","cmd":"cd /home/zzg/coopinion/lemur-4.11/site-search/oopin_cgi_ctr_v2/bin_1;./adhoc_ctr_feeding.o' '1' 12 2016-06-08 12"}
 			self.host2   = {"host_":"123.57.226.182","port_":22,"username":"root","password":"Jksd3344","cmd":"cd /home/itcast/testy;./sleepTest.o"}
 			self.host3   = {"host_":"123.57.226.182","port_":22,"username":"root","password":"Jksd3344","cmd":"cd /home/itcast/testy;./sleepTest.o"}
 
@@ -87,7 +87,7 @@ class TakeShow(object):
 			print("success+id=%s"%self.hostid)
 			days = datetime.timedelta(days=i)
 			self.ShowDays = (Etime-days).strftime("%Y-%m-%d")
-			self.cmd = "cd %s:./adhoc_ctr_feeding.o' '1' %s %s %s"%(self.bin1,str(self.ucid),str(self.ShowDays),str(self.oid))
+			self.cmd = "cd %s;./adhoc_ctr_feeding.o' '1' %s %s %s"%(self.bin1,str(self.ucid),str(self.ShowDays),str(self.oid))
 			print("self.cmd%s"%self.cmd)
 			#通过hostid确定执行的命令和主机ip
 			if self.hostid==1:
