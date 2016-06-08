51b35df62e2a93d4dd1df0c8d9c11174d3f8fd4e
jksd3344 <jiayuguang@qq.com>
2016-06-08 17:12:13 +0800

    2016-06-08 am 50:00

--- z_ReFile/takeshow.py
+++ z_ReFile/takeshow.py
@@ -44,9 +44,9 @@ class TakeShow(object):
 			self.hostid  = int((self.pat)[6])
 			self.sqlcom  = self.sqlcom%(self.uid)
 			self.sqlwh   = self.sqlwh%(self.uid)
-			self.cmd     = './adhoc_ctr_feeding.o','1',str(self.ucid),str(self.ShowDays),str(self.oid)
+			self.cmd     = 'cd /home/zzg/coopinion/lemur-4.11/site-search/oopin_cgi_ctr_v2/bin_1;./adhoc_ctr_feeding.o','1',str(self.ucid),str(self.ShowDays),str(self.oid)
 			print ("self.cmd=%s"%self.cmd)
-			self.host1   = {"host_":"192.168.241.50","port_":17717,"username":"root","password":"Jksd3344","cmd":"cd %s;%s"%(self.bin1,self.cmd)}
+			self.host1   = {"host_":"123.57.226.182","port_":22,"username":"root","password":"Jksd3344","cmd":"cd /home/itcast/testy;./sleepTest.o"}
 			self.host2   = {"host_":"123.57.226.182","port_":22,"username":"root","password":"Jksd3344","cmd":"cd /home/itcast/testy;./sleepTest.o"}
 			self.host3   = {"host_":"123.57.226.182","port_":22,"username":"root","password":"Jksd3344","cmd":"cd /home/itcast/testy;./sleepTest.o"}
 
