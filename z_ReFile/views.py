6754040d3d495915e0071c96db9b049e840ab214
jksd3344 <jiayuguang@qq.com>
2016-06-12 14:51:29 +0800

    2016-06-12 02:51

--- z_ReFile/views.py
+++ z_ReFile/views.py
@@ -88,8 +88,8 @@ def takemassage(request):
 	cmd = "python /home/itcast/0420text/djantext/xinshow/z_ReFile/takeshow.py %s %s %s %s %s %s"%(Stime.strftime("%Y-%m-%d"),Etime.strftime("%Y-%m-%d"),ucid,oid,userid,hostid),
 	subprocess.Popen([
 		"python",
-		# "/home/zzg/feed_tool/xinshow/z_ReFile/takeshow.py",
-		"/home/itcast/0420text/djantext/xinshow/z_ReFile/takeshow.py",
+		"/home/zzg/feed_tool/xinshow/z_ReFile/takeshow.py",
+		# "/home/itcast/0420text/djantext/xinshow/z_ReFile/takeshow.py",
 		Stime.strftime("%Y-%m-%d"),
 		Etime.strftime("%Y-%m-%d"),
 		ucid,
